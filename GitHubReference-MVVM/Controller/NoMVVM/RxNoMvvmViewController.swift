427a9d636f821179a012674a3599cda5c65c9b67
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2021-12-12 01:40:12 +0900

    RxSwiftなしバージョンのStoryboard追加

---
+++ GitHubReference-MVVM/Controller/NoMVVM/RxNoMvvmViewController.swift
@@ -0,0 +1,15 @@
+//
+//  NoMVVM.swift
+//  GitHubReference-MVVM
+//
+//  Created by Hiroaki-Hirabayashi on 2021/12/12.
+//
+
+import NSObject_Rx
+import RxOptional
+import RxSwift
+import UIKit
+
+final class RxNoMvvmViewController: UIViewController, HasDisposeBag {
+    
+}
